38bda86b2614cfa805d3a1ba36208e7e231de103
yuki <wkiki1124@gmail.com>
2021-08-15 02:10:17 +0800

    commit

--- bugets/ViewController.swift
+++ bugets/ViewController.swift
@@ -12,9 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(red: 33/255, green: 33/255, blue: 33/255, alpha: 1)
-        
-     
      
         // yellow
         addRing(percentage: 55, startAngle: 270, red: 239/255, green: 185/255, blue: 0, alpha: 1)
@@ -46,8 +43,8 @@ class ViewController: UIViewController {
         addLabel(percentage: 7, startDegree: 270 + (360 * 92/100))
         addRing(percentage: 1, startAngle: 270 + (360 * 99/100), red: 0, green: 0, blue: 0, alpha: 0)
         
-        
-        
+        // radom color ring
+        addRandomColorRing()
         
          }
     
@@ -62,7 +59,7 @@ class ViewController: UIViewController {
         
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 30))
         label.backgroundColor = UIColor.clear
-        label.font = UIFont.systemFont(ofSize: 26)
+        label.font = UIFont.systemFont(ofSize: 14)
         label.text = "\(percentage)%"
         label.sizeToFit()
         label.center = textPath.currentPoint
@@ -87,6 +84,41 @@ class ViewController: UIViewController {
         percentageLayer.fillColor = UIColor.clear.cgColor
         
         view.layer.addSublayer(percentageLayer)
+        
+    }
+    
+    func addRandomColorRing() {
+        let aDegree = CGFloat.pi/180
+        let lineWidth: CGFloat = 40
+        let radius: CGFloat = 100
+        var startDegree: CGFloat = 270
+        let center = CGPoint(x: 45 + lineWidth + radius, y: 450 + lineWidth + radius)
+        var percentages: [CGFloat] = [25, 15, 7, 33, 20]
+        
+        for percentage in percentages {
+            let endDegree = startDegree + 360 * percentage / 100
+            let percentagePath = UIBezierPath(arcCenter: center, radius: radius, startAngle: aDegree * startDegree, endAngle: aDegree * endDegree, clockwise: true)
+            
+            let percentageLayer = CAShapeLayer()
+            percentageLayer.path = percentagePath.cgPath
+            percentageLayer.strokeColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1).cgColor
+            percentageLayer.fillColor = UIColor.clear.cgColor
+            percentageLayer.lineWidth = lineWidth
+            view.layer.addSublayer(percentageLayer)
+            
+            let textCenterDegree = startDegree + (360 * percentage/100) * 1/2
+            let textPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: aDegree * startDegree, endAngle: aDegree * textCenterDegree, clockwise: true)
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 50, height: 30))
+            label.backgroundColor = UIColor.white
+            label.font = UIFont.systemFont(ofSize: 10)
+            label.text = "\(percentage)%"
+            label.sizeToFit()
+            label.center = textPath.currentPoint
+            view.addSubview(label)
+            
+            startDegree = endDegree
+            
+        }
         
     }
     
